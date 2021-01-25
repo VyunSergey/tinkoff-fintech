92781fca448be055a7ad23bc6c986e52a7b1a90a
Vyun Sergey <vyun.sergey@icloud.com>
2021-01-25 16:48:32 +0300

    fix example

--- src/main/scala/io/github/vyunsergey/tinkoff/fintech/exam/train/Main.scala
+++ src/main/scala/io/github/vyunsergey/tinkoff/fintech/exam/train/Main.scala
@@ -3,11 +3,12 @@ package io.github.vyunsergey.tinkoff.fintech.exam.train
 object Main {
   def main(args: Array[String]): Unit = {
     try {
-      val a = scala.io.StdIn.readInt()
-      val b = scala.io.StdIn.readInt()
+      val in: java.util.Scanner = new java.util.Scanner(System.in)
+      val a: Int = in.nextInt()
+      val b: Int = in.nextInt()
       System.out.print(a + b)
     } catch {
-      case e: Exception => System.err.println("Error: " + e.getMessage);
+      case e: Exception => System.err.println("Error: " + e.getMessage)
     }
   }
 }
